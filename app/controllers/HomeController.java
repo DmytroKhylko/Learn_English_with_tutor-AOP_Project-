1cd37aee75e13b61d67ddc5cc6c01175426f4bf1
DmytroKhylko <0976589260dima@gmail.com>
2020-05-15 19:28:57 +0300

    changed home_page route

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -15,9 +15,7 @@ public class HomeController extends Controller {
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
-    public Result index() {
-        return ok(views.html.index.render());
-    }
+
     public Result home(Http.Request request) {
       return ok(views.html.home.render(request));
     }
